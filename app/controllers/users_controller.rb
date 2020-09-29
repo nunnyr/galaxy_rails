f6462ce681e069c323ee007a5c8ead36ea8d3228
wlcreate <waverley.nyc@gmail.com>
2020-09-28 20:13:03 -0400

    adding trip planet details and user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
         render json: @users
     end
 
+    def create
+        @user = User.create(user_params)
+        render json: @user
+        # if @user.valid?
+        #     render json: @user, status: 201
+        # else
+        #     render json: {errors: @user.errors.full_messages}, status: 404
+        # end
+    end
+
+    # def update
+    # end
+
+    private
+    def user_params
+        params.permit(:first_name, :last_name, :age, :email, :friendly_neighbor)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
 
   get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
 
 
   get '/trips', to: 'trips#index'
