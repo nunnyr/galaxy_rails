f80b6c293c516e40b4e25a62538297e06ad876b7
wlcreate <waverley.nyc@gmail.com>
2020-09-30 09:53:59 -0400

    sending the info for trip from the backend to the frontend

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,10 +5,15 @@ class TripsController < ApplicationController
     end
 
     def create
-        byebug
+        # we are doing .strptime b/c of how our datatype & how params sends the date back
+        new_date = Date.strptime(params[:date], '%Y-%m-%d')
+        new_price = Faker::Number.between(from: 200, to: 500)
+        new_user = params[:user][:id]
+        new_depart_planet = params[:depart_planet_id]
+        new_arrival_planet = params[:arrival_planet_id]
+        
+        @trip = Trip.create(date: new_date, price: new_price, user: new_user, depart_planet_id: new_depart_planet, arrival_planet_id: new_arrival_planet)
 
-
-        @trip = Trip.create(trip_params)
         render json: @trip
     end
 
