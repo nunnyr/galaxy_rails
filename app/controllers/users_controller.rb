e8b20f49114994b2dcc759b6c73e0cbccc6629a1
Nunny R <nunnyreyes@gmail.com>
2020-09-30 18:46:48 -0400

    locating global user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
     # def update
     # end
 
+    def login
+        @found_user = User.find_by(email: params[:theUserNameFromFrontEnd])
+        @found_trip = @found_user.trips
+        # byebug
+        #sorry Eric we kept the same name
+        if @found_user
+            render json: {trips: @found_trip, user: @found_user}
+        # else
+        #     render json: {error: “no user with that name exists”}
+        end
+
+    end
+
+
     private
     def user_params
         params.permit(:pronouns, :first_name, :last_name, :age, :email, :friendly_neighbor)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/users', to: 'users#index'
   post '/users', to: 'users#create'
+  post '/login', to: 'users#login'
 
 
   get '/trips', to: 'trips#index'
